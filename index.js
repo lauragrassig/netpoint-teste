14e61a7a09c3ac5995c85755c18276bb97df7afe
Laura <laura_grassig@outlook.com>
2019-08-04 16:27:52 -0300

    scss finalizado + inicio validações

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+var $qtdSticker = $("#qtdSticker");
+  $qtdSticker.val(0);
+
+$(".btn-counter").click(function () {
+  if ( $(this).hasClass('adicionar'))
+    $qtdSticker.val(parseInt($qtdSticker.val()) +1);
+  else if ($qtdSticker.val() >= 1)
+    $qtdSticker.val(parseInt($qtdSticker.val()) -1);
+})
+
+$('#enviar').bind('click', function() {
+  if( $(".input-style").is(":checked") == false )
+    window.alert('Por favor preencha corretamente os campos');
+
+    return false;
+});
+
+// $("#testForm").submit(function() {
+//   /* Do Something */
+
+// });
